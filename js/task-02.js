fa6b75b93e440b4b15ebfd305e5b59ef7f5814ab
KalinichenkoArtem <110483657+KalinichenkoArtem@users.noreply.github.com>
2022-12-15 18:54:39 +0200

    1

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients");
+
+ingredients.forEach(function (element) {
+  const itemList = document.createElement("li");
+  itemList.textContent = element;
+  itemList.classList.add("item");
+  list.prepend(itemList);
+  console.log(itemList);
+});
