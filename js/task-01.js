56ca67d64bf047ca3427dbdf400989a3683a0812
KalinichenkoArtem <110483657+KalinichenkoArtem@users.noreply.github.com>
2022-12-15 15:48:56 +0200

    1

--- js/task-01.js
+++ js/task-01.js
@@ -1,20 +1,8 @@
 const numberOfCategories = document.querySelectorAll(".item");
 console.log(`Number of categories: ${numberOfCategories.length}`);
-
 console.log("");
 
-const titleOfCategory = document.querySelectorAll("ul h2");
-const list = document.querySelector("li ul");
-const childList = list.children;
-
-// for (const el of titleOfCategory) {
-//     console.log(`Category: ${el.textContent}`);
-//     console.log(`Elements: ${childList.length}`);
-//     console.log("");
-// };
-
-titleOfCategory.forEach(function (element, index) {
-    console.log(`Category: ${element.textContent}`);
-    console.log(`Elements: ${childList.length}`);
-    console.log("");
+numberOfCategories.forEach(function (element) {
+    console.log(`Category: ${element.firstElementChild.textContent}`);
+    console.log(`Category: ${element.lastElementChild.children.length}`);
 });
