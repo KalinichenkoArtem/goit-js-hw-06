21ee2965598ac035ce5aa4422d77af43ed71560e
KalinichenkoArtem <110483657+KalinichenkoArtem@users.noreply.github.com>
2022-12-18 14:12:22 +0200

    1

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,17 @@
+const form = document.querySelector("form");
 
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    
+    const {elements: { email, password }} = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Все поля должны быть запонены!");
+    };
+
+    console.log(`Email: ${email.value}, Password: ${password.value}`);
+  
+    event.currentTarget.reset();
+};
