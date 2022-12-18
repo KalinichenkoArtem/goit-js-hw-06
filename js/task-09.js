d4118cb4976b3592ea4e823398bada39ee6b34ff
KalinichenkoArtem <110483657+KalinichenkoArtem@users.noreply.github.com>
2022-12-18 14:56:56 +0200

    1

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,12 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const body = document.body;
+const span = document.querySelector("span");
+const btn = document.querySelector("button");
+
+btn.addEventListener("click", (event) => {
+  const color = getRandomHexColor();
+  span.textContent = color;
+  body.style.backgroundColor = color;
+});
